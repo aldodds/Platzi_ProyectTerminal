9f2f0f74352ff9a82bec50864af779191175abd7
Aldo DS <aldodaconteb@gmail.com>
2024-07-15 14:13:02 -0500

    cree el script en code debo darle permisos de ejecucion con <chmod +x limpiar_datos.sh> y luego ejecutarlo con ./limpiar_datos.sh

---
+++ limpiar_datos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#definir directorios
+DIR_ENTRADA="datos_entrada"
+DIR_SALIDA="datos_procesados"
+
+#Procesar cada archivo CSV en el directorio de entrada
+for archivo in $DIR_ENTRADA/*.csv
+do
+    nombre_archivo=$(basename "$archivo")
+    echo "Procesanod $nombre_archivo..."
+
+    #Eliminar espacios en blanco al incio de cada línea
+    #Eliminar líneas vacías
+    #Reemplazar campos vacíos con 'N/A'
+
+    cat "$archivo" | sed 's/^[\t]*//' | sed '/^$/d' | sed 's/,,/,N/A,/g; s/,$/,N/A' > "$DIR_SALIDA/${nombre_archivo%.csv}_limpio.csv"
+
+    echo "Archivo limpio guardado como ${nombre_archivo%.csv}_limpio.csv"
+done
+
+echo "Proceso de limpieza completado."
